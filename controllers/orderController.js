d52006e9f0626d8e2cff2fdb8f58760b142ac0cd
Yusuf Samaila <yusuf.hilside@gmail.com>
2024-02-04 20:58:42 +0100

    update

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -48,7 +48,7 @@ exports.addOrder = (req, res, next) => {
       src="https://scontent.fabv2-2.fna.fbcdn.net/v/t39.30808-6/302703516_493834659413950_4926351559681213761_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=783fdb&_nc_eui2=AeGDEMXJJD9pAnNBNVZtI48xwCmsR7KqMHvAKaxHsqowe82ve9AmKixTKrTbts3YmRs&_nc_ohc=NqeWtoyaZqAAX-z1euF&_nc_zt=23&_nc_ht=scontent.fabv2-2.fna&oh=00_AfA6bTNjtjT9ZmrjPpSTgB8g4MPIltPHT3Ga7d6AdJzM4g&oe=65C42B1F"/>
       </div>
         
-  <h2 style="text-align:center"> A new Order </h2>
+  <h2 style="text-align:center"> A new Order</h2>
   <hr/>
   <p><b> Name:</b> ${req.body.firstName} ${req.body.lastName} </p>
   <p><b> Email:</b> ${req.body.email} </p>
@@ -60,7 +60,7 @@ exports.addOrder = (req, res, next) => {
   <p> <b>Total:</b> ₦${Number(req.body.amount) * Number(req.body.price)} </p>
   <hr/>
 
-  <b><i><p style="font-size:18px;">Skimake is saying Thank you for your patronage</p></i></b>
+  <b><i><p style="font-size:18px;">Thank you for your patronizing Skimake.</p></i></b>
 
   </div>
     </body>
@@ -77,7 +77,8 @@ exports.addOrder = (req, res, next) => {
 
   var mailOptions = {
     from: "yusuf.hilside@gmail.com",
-    to: "yusuf.hilside@gmail.com",
+    to: "info@skimake.com",
+    bcc: req.body.email,
     subject: "A new Skimake Order",
     html: emailBody,
   };
