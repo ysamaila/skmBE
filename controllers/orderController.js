f4bf15ba4dfd94f9db6e981880cc740e0713d608
Yusuf Samaila <yusuf.hilside@gmail.com>
2024-02-04 21:12:40 +0100

    update

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -48,7 +48,7 @@ exports.addOrder = (req, res, next) => {
       src="https://scontent.fabv2-2.fna.fbcdn.net/v/t39.30808-6/302703516_493834659413950_4926351559681213761_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=783fdb&_nc_eui2=AeGDEMXJJD9pAnNBNVZtI48xwCmsR7KqMHvAKaxHsqowe82ve9AmKixTKrTbts3YmRs&_nc_ohc=NqeWtoyaZqAAX-z1euF&_nc_zt=23&_nc_ht=scontent.fabv2-2.fna&oh=00_AfA6bTNjtjT9ZmrjPpSTgB8g4MPIltPHT3Ga7d6AdJzM4g&oe=65C42B1F"/>
       </div>
         
-  <h2 style="text-align:center"> A new Order</h2>
+  <h2 style="text-align:center"> A new Order (Dev Test)</h2>
   <hr/>
   <p> <b>State:</b> Mug Cup </p>
   <p><b> Name:</b> ${req.body.firstName} ${req.body.lastName} </p>
