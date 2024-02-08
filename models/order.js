70eb259f04463819193ecbd743ba238e46ea90f0
Yusuf Samaila <yusuf.hilside@gmail.com>
2024-02-08 13:58:09 +0100

    Updated

--- models/order.js
+++ models/order.js
@@ -4,17 +4,20 @@ const uniqueValidator = require("mongoose-unique-validator");
 //defining business details schema
 const orderSchema = mongoose.Schema(
   {
-    email: { type: String, required: true },
-    phone: { type: String, required: true },
+    email: { type: String, required: false },
+    phone: { type: String, required: false },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    address: { type: String, required: true },
-    city: { type: String, required: true },
-    state: { type: String, required: true },
-    amount: { type: Number, required: true },
-    price: { type: Number, required: true },
-    country: { type: String, required: true },
-    totalPaid: { type: Number, required: true },
+    address: { type: String, required: false },
+    city: { type: String, required: false },
+    state: { type: String, required: false },
+    amount: { type: Number, required: false },
+    price: { type: Number, required: false },
+    country: { type: String, required: false },
+    totalPaid: { type: Number, required: false },
+    location: { type: String, required: false },
+    item: { type: String, required: true },
+    image: { type: String, required: false },
   },
   { timestamps: true }
 );
